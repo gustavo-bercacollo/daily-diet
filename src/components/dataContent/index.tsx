683b5ab28eacfeb1a8ac1b55b06ccc25e568fee1
Gustavo Berçacollo Vilela <gustavovilela802@gmail.com>
2025-01-14 16:12:05 -0300

    add info meal statistics component

--- src/components/dataContent/index.tsx
+++ src/components/dataContent/index.tsx
@@ -1,7 +1,12 @@
+import { Sequence } from "@components/sequence";
 import { Container } from "./styles";
+import { Total } from "@components/total";
 
 export function DataContent() {
   return (
-    <Container/>
+    <Container>
+      <Sequence/>
+      <Total/>
+    </Container>
   );
 }
